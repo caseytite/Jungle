0c267b9ff95c89ccd0c1bf3a58306f352e38bbc2
Casey <caseyjtite@gmail.com>
2022-03-16 19:47:40 -0700

    add screenshots

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class Admin::DashboardController < ApplicationController
 
-  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"], password: ENV["HTTP_BASIC_PASSWORD"], if: -> { ENV["HTTP_BASIC_PASSWORD"].present? }
+  
+  http_basic_authenticate_with :name => ENV["HTTP_BASIC_USER"], :password => ENV["HTTP_BASIC_PASSWORD"]
+  # before_filter :authorize
 
   def show
     @products = Product.all
